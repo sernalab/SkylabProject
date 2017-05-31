5e187e334d842f298b2ada7744cf97f3e0c18277
Carles Serna <carles.serna@gmail.com>
2017-05-31 16:32:08 +0200

    students added

--- client/js/ekko-lightbox.js
+++ client/js/ekko-lightbox.js
@@ -301,7 +301,7 @@ License: https://github.com/ashleydw/lightbox/blob/master/LICENSE
                 return true;
             }
             if (typeof match[2] === "string" && match[2].length > 0 && match[2].replace(new RegExp(":(" + {
-                    "https:": 80,
+                    "http:": 80,
                     "https:": 443
                 }[location.protocol] + ")?$"), "") !== location.host) {
                 return true;
