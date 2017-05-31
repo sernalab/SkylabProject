07056513176508f0ea15ab73ad5813e63ca0c878
Carles Serna <carles.serna@gmail.com>
2017-05-31 15:49:33 +0200

    .env path fixed

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+require('dotenv').load()
+
+const PORT = process.env.PORT
+
+app.use(express.static(path.join(__dirname, '../client')))
+
+app.listen(PORT, () => console.log(`B-) SkylabProjects running on PORT ${PORT}`))
