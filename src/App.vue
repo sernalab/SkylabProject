b2993931a3e4888f0ba48c333532f32091894947
Carles Serna <carles.serna@gmail.com>
2017-06-13 15:22:39 +0200

    w/o fonts awesome, and more details

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,6 @@ export default {
 <style href="https://fonts.googleapis.com/css?family=Roboto:400,300,700,400italic"></style>
 
 <!-- projects -->
-<style src="../static/css/font-awesome.min.css"></style>
 <style src="../static/css/bootstrap.css"></style>  
 <style src="../static/css/style.default.css" lang="css"></style>
 
