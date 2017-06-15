dc27c2e7b104867627afa82d62d681cb8914c22f
Carles Serna <carles.serna@gmail.com>
2017-06-15 23:22:36 +0200

    trying to fix heroku-2

--- server/app.js
+++ server/app.js
@@ -22,14 +22,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect('http://' + req.hostname + req.url);
-  } else {
-    next();
-  }
-})
-
 app.use('/api/users', routesUsers)
 
 module.exports = app
