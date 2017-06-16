3e79db06013710a4441a15e9fa8c1d3502de9656
Carles Serna <carles.serna@gmail.com>
2017-06-16 13:12:50 +0200

    fixing github-repo

--- server/models/Users.js
+++ server/models/Users.js
@@ -38,7 +38,7 @@ const UserSchema = new mongoose.Schema({
             email: {
                 type: String
             },
-            github: {
+            github_repo: {
                 type: String
             },
             description: {
